7a5391cf31570ef9fae347553bda9afff79f8b74
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-05-01 03:29:02 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -3,6 +3,6 @@
 def parrot (phrase = "Squawk!")
  #puts "Squawk!"#test 1: output "Squawk!" when called w/o argument
  #"Squawk!" #test 2: return default phrase when called w/o argument
- puts phrase #test 3: output given phrase when called w/ argument
- phrase #test 4: return given phrase when called w/ argument
+ puts phrase #test 3: output given phrase when called w/ or w/o argument
+ phrase #test 4: return given phrase when called w/ or w/o argument
 end
