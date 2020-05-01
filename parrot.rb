37719d4a6341a3bae7b2a459217783bdf4d4cb24
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-05-01 03:24:39 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot
- puts "Squawk!"
+ puts "Squawk!"#test 1: output "Squawk!" when called w/o argument
 # print phrase
-# "Squawk!" #test 2
+ "Squawk!" #test 2: return default phrase when called w/o argument
 end
